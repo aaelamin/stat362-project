d5242e5cf5227e67593182b1b7789715513854c6
Adam Elamin <116977132+aaelamin@users.noreply.github.com>
2024-03-29 17:07:02 -0400

    odified proj.R

---
+++ proj.R
@@ -0,0 +1,11 @@
+# Project Title: Your Project Name Here
+# Description: Fetal Health Analysis: Predictive Modeling Using CTG Data
+# Authors: Adam Elamin, Martina Mileva, Roy Yeoun, Basil Abdelhadi, Jasmine Taylor
+# Date: 29 March 2024
+
+# Import necessary libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+df <- read.csv("fetal_health.csv")
